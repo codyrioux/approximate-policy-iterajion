66f042886fae83d309a7b580b936f72ab58bc586
Cody Rioux <cody.rioux@gmail.com>
2013-05-31 15:04:07 -0600

    Algorithm is working, sample solution is complete and a test was run to show that learning took place (with statistical significance).

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject approximate-policy-iterajion "0.0.1"
+( defproject approximate-policy-iterajion "0.0.1"
   :description "An implementation of Approximate Policy Iteration algorithm in Lagoudakis et al. 2003."
   :url "https://www.github.com/codyrioux/approximate-policy-iterajion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [svm-clj "0.1.0"]
-                 [incanter/incanter-core "1.5.0-SNAPSHOT"]])
+                 [incanter/incanter-core "1.5.0-SNAPSHOT"]]
+  :main approximate-policy-iterajion.sample)
--- src/approximate_policy_iterajion/sample.clj
+++ src/approximate_policy_iterajion/sample.clj
@@ -1,70 +1,46 @@
 (ns approximate-policy-iterajion.sample
   "A sample problem for approximate policy iterajion
-   in which we attempt to find the number 100.
+   in which we attempt to find the goal number.
    
    States are defined as just a number.
    
    Actions are a number which will be added to the state."
-  (:require [approximate-policy-iterajion.core :as api]))
+  (:require [approximate-policy-iterajion.core :as api])
+  (:gen-class))
+
+(def goal 10)
 
 ;; Experiment Functions
 
 (defn m
-  "States and actions are added.
-   Reward is 1 / (|(100 - (s + a))| + 0.01)"
+  "States and actions are added. Reward is 1 / (|(goal - (s + a))| + 0.01)"
   [s a]
-;  (if (or (not (number? s)) (not (number? a))) (println (str "Called with s: " s " and a: " a)))
-  [(+ s a)  (/ 1 (+ 0.01 (Math/abs (- 100 (+ s a))))) ])
+  [(+ s a)  (/ 1 (+ 0.01 (Math/abs (- goal (+ s a))))) ])
 
 (defn dp
-  "1000 initial states somewhere from 0 to 200"
+  "0 to goal * 2 for starting states"
   []
-  (repeat 10 (rand-int 200)))
+  (range 0 (* goal 2)))
 
 (defn sp
-  "At any state we can add anywhere from -10 to 10.
-   This is arbitrarily chosen."
+  "Can add or subtract up to half of the goal."
   [s]
-  (range -10 11))
+  (range (* -1 (/ goal 2)) (/ goal 2)))
 
 (defn features
-  "Features are the value of the state and the difference from 100 (the target)"
+  "Features are the value of the state and the difference from goal"
   [s]
-  {1 s, 2 (Math/abs (- 100 s))})
+  {1 s
+   2 (- goal s)
+   3 (if (pos? s) 1 0)
+   4 (if (pos (- goal s) 1 0))
+   5 (if (> goal s) 1 0)
+   })
 
 ;; Utility Functions
 
 (defn create-api-policy
-  "Build a function that implements our policy."
+  "Build a function that implements our policy.
+   The returned function can be called with a state and will recommend an action."
   [trajectory-count trajectory-length]
-  (api/api m dp sp 0.5 (partial api/policy features) trajectory-count trajectory-length features))
-
-(defn create-random-policy
-  "Build a random policy, for benchmarking our function."
-  []
-  (fn [x] (rand-nth (sp 1))))
-
-(defn find-100
-  "Returns the number of actions necessary to find 100 using policy.
-   After 1000 iterations it fails out."
-  [policy]
-  (loop [state 0 t 0]
-    (cond
-      (= 100 state) t
-      (= t 1000) t
-      :else (recur (+ state (policy state)) (+ t 1)))))
-
-(defn run-experiment
-  "Tries to find 100 with the policy n times, returning a collection
-   containing the number of iterations necessary."
-  [policy n]
-  (doall (map (fn [x] (find-100 policy)) (range n))))
-
-(defn does-api-beat-random?
-  "Returns true if the mean number of iterations for our policy
-   is statistically significant in comparison to a random policy."
-  []
-  (let [api-results (run-experiment (create-api-policy 5 2) 10)
-        random-results (run-experiment (create-random-policy) 10)
-        api-mean (/ (reduce + api-results) (count api-results))]
-    (api/statistically-significant? identity 0.05 random-results api-mean)))
+  (api/api m dp sp 0.99 (partial api/policy features) trajectory-count trajectory-length features))
