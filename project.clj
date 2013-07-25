60bfd1ced1729d6737da27ff10914d404ac30897
codyrioux <cody.rioux@gmail.com>
2013-07-25 10:31:59 -0600

    Updated project dependencies to remove SNAPSHOTs, updated documentation, cleaned up testing (need more tests).

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://www.github.com/codyrioux/approximate-policy-iterajion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [svm-clj "0.1.2"]
-                 [incanter/incanter-core "1.5.0-SNAPSHOT"]]
+                 [incanter/incanter-core "1.5.1"]]
   :plugins [[lein-marginalia "0.7.1"]])
--- test/approximate_policy_iterajion/core_test.clj
+++ test/approximate_policy_iterajion/core_test.clj
@@ -16,6 +16,7 @@
                    ["x" -20]
                    ["z" 20]])
 
+(def statistically-significant? (ns-resolve 'approximate-policy-iterajion.core 'statistically-significant?))
 (deftest statistically-singificant-test
   (testing "statistically-significant?")
   (is (= true (statistically-significant? identity 0.05 (range 1 10) 12)))
@@ -24,15 +25,3 @@
   (is (= true (statistically-significant? second 0.05 test-samples (last test-samples))))
   (is (= true (statistically-significant? second 0.05 test-samples (nth test-samples 11))))
   (is (= false (statistically-significant? second 0.05 test-samples (nth test-samples 2)))))
-
-(def get-positive-samples (ns-resolve 'approximate-policy-iterajion.core 'get-positive-samples))
-(deftest get-positive-samples-test
-  (testing "get-positive-samples"
-    (is (= #{[1 ["z" 20]]} (get-positive-samples test-samples 20)))
-    (is (= #{} (get-positive-samples test-samples 5)))))
-
-(def get-negative-samples (ns-resolve 'approximate-policy-iterajion.core 'get-negative-samples))
-(deftest get-negative-samples-test
-  (testing "get-negative-samples"
-    (is (= #{[-1 ["x" -20]]} (get-negative-samples test-samples)))
-    (is (= #{} (get-negative-samples (take 5 test-samples))))))
